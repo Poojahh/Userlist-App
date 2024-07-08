0ada094ffa2c1ae768cbeb16acea575b8cd91f85
Poojahh <poojakrishnan1104@gmail.com>
2024-07-08 10:38:51 +0530

    Userlist App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/AddUser';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchUser from './components/SearchUser';
 
 function App() {
   return (
     <div>
-      <AddUser/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddUser/>}/>
+        <Route path="/search" element={<SearchUser/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
