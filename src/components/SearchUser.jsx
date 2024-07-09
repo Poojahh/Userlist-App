24503afa380da86370b3536e54bc9feba453c150
Poojahh <poojakrishnan1104@gmail.com>
2024-07-09 21:02:40 +0530

    Userlist App

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -5,7 +5,7 @@ const SearchUser = () => {
   return (
     <div>
         <Navbar/>
-      <div className="contaniner">
+      <div className="container">
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <div className="row g-3">
