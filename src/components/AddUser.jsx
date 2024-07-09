f47051b9be43d24b3ab4a42261208fd486c6af98
Poojahh <poojakrishnan1104@gmail.com>
2024-07-09 20:57:19 +0530

    Userlist App

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -4,7 +4,8 @@ import Navbar from './Navbar'
 const AddUser = () => {
   return (
     <div>
-      <div className="contaniner">
+        <Navbar/>
+      <div className="container">
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <div className="row g-3">
--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const SearchUser = () => {
   return (
     <div>
+        <Navbar/>
       <div className="contaniner">
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
